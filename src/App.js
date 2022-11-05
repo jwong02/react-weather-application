be3f64411f311495a285593740d9a923e5bb522b
Janet <janetwong024@gmail.com>
2022-11-05 11:14:37 -0400

    created a weather component and added html structure; created weather css file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        Weather App
+        <Weather />
         <footer>
           {" "}
           This project was coded by Janet Wong and is {""}
---
+++ src/Weather.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form id="search-form" class="mb-3 mt-2">
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="search for a city"
+              autocomplete="off"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-info w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Last Updated on Saturday 12:00</li>
+      </ul>
+      <div className="row">
+        <div className="col-4">
+          <ul>
+            <li>
+              <span className="main-temp">70</span>
+              <span className="units">°</span>
+            </li>
+          </ul>
+        </div>
+        <div className="col-4">
+          <ul className="high-low-section">
+            <li>
+              <span className="current-high-low">High:</span>
+              <span> 80</span>
+              <span className="high-low-units">°</span>
+            </li>
+            <li>
+              <span className="current-high-low">Low:</span>
+              <span> 60</span>
+              <span className="high-low-units">°</span>
+            </li>
+          </ul>
+        </div>
+        <div className="col-4">
+          <ul>
+            <li>
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                alt=""
+                className="main-weather-icon"
+              />
+            </li>
+            <li className="current-condition">Sunny</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}

f53948ea47b8e29f4c5c518bec6e8399249b9b51
Janet <janetwong024@gmail.com>
2022-11-05 10:48:30 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        {" "}
-        This project was coded by Janet Wong and is {""}
-        <a
-          href="https://github.com/jwong02/react-weather-application"
-          target="_blank"
-        >
-          open-sourced on GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          {" "}
+          This project was coded by Janet Wong and is {""}
+          <a
+            href="https://github.com/jwong02/react-weather-application"
+            target="_blank"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

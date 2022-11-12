b09cd76f0614c9965134387366375ce92b9507ef
Janet <janetwong024@gmail.com>
2022-11-12 16:03:26 -0500

    updated weather icon URL - connected to api

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props) {
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconURL: `https://ssl.gstatic.com/onebox/weather/64/sunny.png`,
+      iconURL: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
 
     setReady(true);

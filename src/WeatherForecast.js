4e70e9f6d9bc8344786b5f3f61e6aa3b748d7ae9
Janet <janetwong024@gmail.com>
2022-11-24 13:22:48 -0500

    created weather forecast component and added css

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,10 +1,17 @@
 import React from "react";
+import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
 
 export default function WeatherForecast() {
   return (
     <div className="WeatherForecast">
       <div className="row">
-        <div className="col">Thur Icon 19 10</div>
+        <div className="col">
+          <div className="WeatherForecast-day mb-2">Thu</div>
+          <WeatherIcon code="01d" size={30} />
+          <div className="WeatherForcast-maxTemp mt-1">45°</div>
+          <div className="WeatherForcast-minTemp">30°</div>
+        </div>
       </div>
     </div>
   );
--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="black"
-      size={65}
+      size={props.size}
       animate={true}
     />
   );
